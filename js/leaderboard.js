770e0950a89dd6403e514ae63e0748f9bebed99d
Jackson Willis <jackson@jacksonwillis.com>
2014-02-18 12:15:35 +0000

    score modification function

---
+++ js/leaderboard.js
@@ -0,0 +1,31 @@
+function addPoints(points) {
+	var score = $("#score");
+	var scoreVal = parseInt(score.text()) + parseInt(points);
+	score.html('<h1>' + scoreVal + '</h1>');
+}
+
+function getLeaderboard() {
+	$.ajax({
+		url: 'http://jacksonwillis.com:3000/scores',
+		type: "GET",
+		dataType: "json",
+		success: function (data) {
+		    // console.log(data);
+		    for (index = 0; index < data.length; ++index) {
+			    $("#scoreboard").append( "<p>"+data[index].name+" - "+data[index].score+"</p>" );
+			}
+		}
+	});
+}
+
+function postScore(name, score) {
+	$.ajax({
+		url: 'http://jacksonwillis.com:3000/score',
+		type: "POST",
+		dataType: "json",
+		data : "",
+		success: function (data) {
+		    console.log(data);
+		}
+	});
+}
