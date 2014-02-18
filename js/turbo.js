2405744f2950a34849259b7b590a5da6bbe06a8b
Jackson Willis <jackson@jacksonwillis.com>
2014-02-18 13:54:57 +0000

    turbomoad fireworks

--- js/turbo.js
+++ js/turbo.js
@@ -1,11 +1,11 @@
 function turboModeOn() {
-	$("#logo").toggle();
 	$("#turbo").toggle();
 	$('body').css("background-color","black");
+	var fworks = new Fireworks();
 }
 
 function turboModeOff() {
-
+	$("#turbo").toggle();
 }
 
 var Fireworks = function(){
@@ -395,11 +395,9 @@ var Fireworks = function(){
 	self.init();
   
   var initialLaunchCount = 10;
-  while(initialLaunchCount--){
-    setTimeout(function(){
+    setInterval(function(){
  		self.fireworks.push(new Firework(self.cw/2, self.ch, rand(50, self.cw-50), rand(50, self.ch/2)-50));
-    }, initialLaunchCount*200);
-  }
+    }, initialLaunchCount);
 	
 }
 
@@ -574,40 +572,3 @@ var guiPresets = {
 				}
 			  }
 			};
-
-
-var fworks = new Fireworks();
-var gui = new dat.GUI({
-	autoPlace: false,
-	load: guiPresets,
-	preset: 'Default'
-});
-var customContainer = document.getElementById('gui');
-customContainer.appendChild(gui.domElement);
-
-var guiFireworks = gui.addFolder('Fireworks');
-guiFireworks.add(fworks, 'fworkSpeed').min(1).max(10).step(1);
-guiFireworks.add(fworks, 'fworkAccel').min(0).max(50).step(1);
-guiFireworks.add(fworks, 'showShockwave');
-guiFireworks.add(fworks, 'showTarget');
-
-var guiParticles = gui.addFolder('Particles');
-guiParticles.add(fworks, 'partCount').min(0).max(500).step(1);	
-guiParticles.add(fworks, 'partSpeed').min(1).max(100).step(1);
-guiParticles.add(fworks, 'partSpeedVariance').min(0).max(50).step(1);
-guiParticles.add(fworks, 'partWind').min(0).max(100).step(1);
-guiParticles.add(fworks, 'partFriction').min(0).max(50).step(1);
-guiParticles.add(fworks, 'partGravity').min(-20).max(20).step(1);
-guiParticles.add(fworks, 'flickerDensity').min(0).max(50).step(1);
-
-var guiColor = gui.addFolder('Color');
-guiColor.add(fworks, 'hueMin').min(0).max(360).step(1);
-guiColor.add(fworks, 'hueMax').min(0).max(360).step(1);
-guiColor.add(fworks, 'hueVariance').min(0).max(180).step(1);
-
-var guiOther = gui.addFolder('Other');
-guiOther.add(fworks, 'lineWidth').min(1).max(20).step(1);
-guiOther.add(fworks, 'clearAlpha').min(0).max(100).step(1);
-guiOther.add(fworks, 'clear').name('Clear');
-
-gui.remember(fworks);
