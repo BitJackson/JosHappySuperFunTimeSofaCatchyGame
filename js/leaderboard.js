80921c057be16b8a43f5ae9e5268abb554fa3917
Jackson Willis <jackson@jacksonwillis.com>
2014-02-18 12:52:04 +0000

    leaderboard posting

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -10,20 +10,21 @@ function getLeaderboard() {
 		type: "GET",
 		dataType: "json",
 		success: function (data) {
-		    // console.log(data);
+		    // console.log(data);		
 		    for (index = 0; index < data.length; ++index) {
-			    $("#scoreboard").append( "<p>"+data[index].name+" - "+data[index].score+"</p>" );
+		    	console.log(data[index].name+" - "+data[index].score);
+			    // $("#scoreboard").append( "<p>"+data[index].name+" - "+data[index].score+"</p>" );
 			}
 		}
 	});
 }
 
-function postScore(name, score) {
+function postScore(name) {
 	$.ajax({
 		url: 'http://jacksonwillis.com:3000/score',
 		type: "POST",
-		dataType: "json",
-		data : "",
+		contentType: "application/json",
+		data : '{"score":"'+$("#score").text()+'","name":"'+name+'"}',
 		success: function (data) {
 		    console.log(data);
 		}
