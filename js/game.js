2b6b6df6e11179190f650d6b8840778d354d3094
Nathan Gaskin <nathan@appliedbrilliance.co.uk>
2014-02-18 10:26:54 +0000

    Adding random laptops all over this shit

--- js/game.js
+++ js/game.js
@@ -27,6 +27,33 @@ function tick(event) {
 	var rand = Math.floor((Math.random()*100)+1);
 	if(rand > 99) {
 		//Create a new 'collidable object'
+		var image = new Image();
+		image.src = "assets/images/laptop.png";
+
+		//This "spritesheet" only has one frame at the moment, so bodging it
+		var spriteSheet = new createjs.SpriteSheet({
+			images: [image],
+			frames: {width: 150, height: 123, regX: 0, regY: 0},
+			animations: {
+				move: [0, "move"]
+			}
+		});
+
+		var bmpAnimation = new createjs.BitmapAnimation(spriteSheet);
+		bmpAnimation.gotoAndPlay("move");
+		bmpAnimation.direction = 180;
+		bmpAnimation.vY = 4;
+		bmpAnimation.x = Math.floor((Math.random() * 500) +1);
+		bmpAnimation.y = 0;
+
+		bmpAnimation.currentFrame = 0;
+		collidables.push(bmpAnimation);
+		stage.addChild(bmpAnimation);
+	}
+
+	for (var i = 0; i < collidables.length; i++) {
+		var collidable = collidables[i];
+		collidable.y += collidable.vY;
 	}
 
 	stage.update(event);
