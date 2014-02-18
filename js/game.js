fc691e964fa4515440e04d5d647aa18d0d6769cf
Jackson Willis <jackson@jacksonwillis.com>
2014-02-18 18:46:10 +0000

    drunk mode off

--- js/game.js
+++ js/game.js
@@ -157,11 +157,11 @@ function tick(event) {
 					hand.alpha = 1.0;
 					addPoints(500);
 					collidable.hasCollided = true;
-                                        player.fadeDown('BACKGROUND_MUSIC', 0.2);
-                                        player.play('COLLECT')
-                                        setTimeout(function() {
-                                           player.fadeUp('BACKGROUND_MUSIC', 1); 
-                                        }, 800);
+                    player.fadeDown('BACKGROUND_MUSIC', 0.2);
+                    player.play('COLLECT')
+                    setTimeout(function() {
+                       player.fadeUp('BACKGROUND_MUSIC', 1); 
+                    }, 800);
 
 					if(collidable.isBooze && !isDrunk && !isTurbo) {
 						drunk++;
@@ -264,9 +264,11 @@ function updateDrunkMode() {
 	setDrunk(drunk);
 	if(drunk == 0) {
             isDrunk = 0;
+			$("#mode").hide();
             clearInterval(drunkModeInterval);
             player.normalTime('BACKGROUND_MUSIC');
             $('#mainCanvas').toggleClass('drunk');
+
 	}
 }
 
