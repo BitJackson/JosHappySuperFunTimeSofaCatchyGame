8db7d3b78409d686c638bffbcb705ecd8910e17b
Nathan Gaskin <nathan@appliedbrilliance.co.uk>
2014-02-18 17:34:17 +0000

    Added game over condition

--- js/game.js
+++ js/game.js
@@ -8,6 +8,7 @@ var drunk = 0;
 var name = 'Player1';
 var health = 25;
 var frequency = 1; //Number of collidables to spawn (% probability on each tick);
+var gameOver = 0;
 
 
 //Drunk mode shit
@@ -65,163 +66,168 @@ function init() {
 }
 
 function tick(event) {
-        if (isDrunk) {
-            var mid = stage.canvas.width / 2
-            if (handPosition > mid) {
-                var diff = handPosition - mid;
-                hand.x = mid - diff;
-            } else {
-                var diff = mid - handPosition;
-                hand.x = mid + diff;
-            }
-        } else {
-            hand.x = handPosition;
-        }
 
-	var rand = Math.floor((Math.random()*100)+1);
-	if(rand > (100 - frequency)) {
-		//Create a new 'collidable object'
-		var image = new Image();
+	if(!gameOver) {
+	    if (isDrunk) {
+	        var mid = stage.canvas.width / 2
+	        if (handPosition > mid) {
+	            var diff = handPosition - mid;
+	            hand.x = mid - diff;
+	        } else {
+	            var diff = mid - handPosition;
+	            hand.x = mid + diff;
+	        }
+	    } else {
+	        hand.x = handPosition;
+	    }
 
-		var rand2 = Math.floor((Math.random()*100)+1);
+		var rand = Math.floor((Math.random()*100)+1);
+		if(rand > (100 - frequency)) {
+			//Create a new 'collidable object'
+			var image = new Image();
 
-		var spriteSheet;
+			var rand2 = Math.floor((Math.random()*100)+1);
 
-		if(rand2 < 20) {
-			image.src = "assets/images/laptop.png";
-			imgWidth = 200;
-			imgHeight = 191;
-		} else if(rand2 >= 20 && rand2 < 40) {
-			image.src = "assets/images/cat.png";
-			imgWidth = 167;
-			imgHeight = 200;
+			var spriteSheet;
 
-		} else if(rand2 >= 40 && rand2 < 50) {
-			image.src = "assets/images/mug.png";
-			imgWidth = 119;
-			imgHeight = 119;
+			if(rand2 < 20) {
+				image.src = "assets/images/laptop.png";
+				imgWidth = 200;
+				imgHeight = 191;
+			} else if(rand2 >= 20 && rand2 < 40) {
+				image.src = "assets/images/cat.png";
+				imgWidth = 167;
+				imgHeight = 200;
 
-		} else if(rand2 >= 50 && rand2 < 75) {
-			image.src = "assets/images/wine.png";
-			imgWidth = 62;
-			imgHeight = 201;
-		} else {
-			image.src = "assets/images/gin.png";
-			imgWidth = 98;
-			imgHeight = 200;
-		}
+			} else if(rand2 >= 40 && rand2 < 50) {
+				image.src = "assets/images/mug.png";
+				imgWidth = 119;
+				imgHeight = 119;
 
-		var spriteSheet = new createjs.SpriteSheet({
-			images: [image],
-			frames: {width: imgWidth, height: imgHeight, regX: 0, regY: 0},
-			animations: {
-				move: [0, "move"]
+			} else if(rand2 >= 50 && rand2 < 75) {
+				image.src = "assets/images/wine.png";
+				imgWidth = 62;
+				imgHeight = 201;
+			} else {
+				image.src = "assets/images/gin.png";
+				imgWidth = 98;
+				imgHeight = 200;
 			}
-		});
 
-		var bmpAnimation = new createjs.Sprite(spriteSheet);
-		bmpAnimation.gotoAndPlay("move");
-		bmpAnimation.direction = 180;
-		bmpAnimation.vY = 4;
-		bmpAnimation.x = Math.floor((Math.random() * stage.canvas.width) +1);
-		bmpAnimation.y = 0;
+			var spriteSheet = new createjs.SpriteSheet({
+				images: [image],
+				frames: {width: imgWidth, height: imgHeight, regX: 0, regY: 0},
+				animations: {
+					move: [0, "move"]
+				}
+			});
 
-		bmpAnimation.currentFrame = 0;
-		if(rand2 >= 50) {
-			bmpAnimation.isBooze = true;
-		} else {
-			bmpAnimation.isBooze = false;
+			var bmpAnimation = new createjs.Sprite(spriteSheet);
+			bmpAnimation.gotoAndPlay("move");
+			bmpAnimation.direction = 180;
+			bmpAnimation.vY = 4;
+			bmpAnimation.x = Math.floor((Math.random() * stage.canvas.width) +1);
+			bmpAnimation.y = 0;
+
+			bmpAnimation.currentFrame = 0;
+			if(rand2 >= 50) {
+				bmpAnimation.isBooze = true;
+			} else {
+				bmpAnimation.isBooze = false;
+			}
+
+			if(rand2 >= 40 && rand2 < 50) {
+				bmpAnimation.givesLife = true;
+			} else {
+				bmpAnimation.givesLife = false;
+			}
+			collidables.push(bmpAnimation);
+			stage.addChild(bmpAnimation);
 		}
 
-		if(rand2 >= 40 && rand2 < 50) {
-			bmpAnimation.givesLife = true;
-		} else {
-			bmpAnimation.givesLife = false;
+		var indicesToRemove = new Array() //Array of indices to remove from collidables
+		for (var i = 0; i < collidables.length; i++) {
+			var collidable = collidables[i];
+			collidable.y += collidable.vY;
+			collidable.rotation += 2;
+
+
+			if (ndgmr.checkPixelCollision(collidable,hand)) {
+				if(!collidable.hasCollided) {
+					hand.alpha = 1.0;
+					addPoints(500);
+					collidable.hasCollided = true;
+
+					if(collidable.isBooze && !isDrunk && !isTurbo) {
+						drunk++;
+						setDrunk(drunk);
+					}
+
+					stage.removeChild(collidable);
+					indicesToRemove.push(i);
+					
+					if(!isTurbo && !isDrunk) {
+						streak++;
+						setStreak(streak);
+					}
+
+					if(collidable.givesLife && health < 100) {
+						health += 5;
+						setHealth(health);
+					}
+				}
+				//console.log('collided');
+			}
+
+			if (collidable.y >= stage.canvas.height) {
+				if(!collidable.hasCollided) {
+					addPoints(-500);
+					collidable.hasCollided = true;
+					stage.removeChild(collidable);
+					indicesToRemove.push(i);
+					
+					if(!isTurbo) {
+						health = health - 5;
+						setHealth(health);
+					}
+
+					if(health == 0) {
+						gameOver();
+						return;
+					}
+					
+					if(!isTurbo) {
+						streak = 0;
+						setStreak(streak);
+					}
+				}
+			}
 		}
-		collidables.push(bmpAnimation);
-		stage.addChild(bmpAnimation);
+
+		/*
+		for (var i = 0; i < indicesToRemove.length; i++) {
+			collidables.splice(indicesToRemove[i],1);
+		}*/
+	        
+	    if (streak == 10 && !isTurbo) {
+	    	console.log('turning on turbo');
+	    	if(drunk == 10) {
+	    		drunk = 9;
+	    	}
+	        toasty.toastIt();
+	        turboModeOn();
+	        //Fire super mode
+	    }
+
+	    if (drunk == 10 && !isDrunk) {
+	        //drunk = 0;
+	        setDrunk(drunk);
+	        makeDrunkModeGo();
+	        //Fire drunk mode
+	    }
+	} else {
+		//Do nothing		
 	}
-
-	var indicesToRemove = new Array() //Array of indices to remove from collidables
-	for (var i = 0; i < collidables.length; i++) {
-		var collidable = collidables[i];
-		collidable.y += collidable.vY;
-		collidable.rotation += 2;
-
-
-		if (ndgmr.checkPixelCollision(collidable,hand)) {
-			if(!collidable.hasCollided) {
-				hand.alpha = 1.0;
-				addPoints(500);
-				collidable.hasCollided = true;
-
-				if(collidable.isBooze && !isDrunk && !isTurbo) {
-					drunk++;
-					setDrunk(drunk);
-				}
-
-				stage.removeChild(collidable);
-				indicesToRemove.push(i);
-				
-				if(!isTurbo && !isDrunk) {
-					streak++;
-					setStreak(streak);
-				}
-
-				if(collidable.givesLife && health < 100) {
-					health += 5;
-					setHealth(health);
-				}
-			}
-			//console.log('collided');
-		}
-
-		if (collidable.y >= stage.canvas.height) {
-			if(!collidable.hasCollided) {
-				addPoints(-500);
-				collidable.hasCollided = true;
-				stage.removeChild(collidable);
-				indicesToRemove.push(i);
-				
-				if(!isTurbo) {
-					health = health - 5;
-					setHealth(health);
-				}
-
-				if(health == 0) {
-					gameOver();
-					return;
-				}
-				
-				if(!isTurbo) {
-					streak = 0;
-					setStreak(streak);
-				}
-			}
-		}
-	}
-
-	/*
-	for (var i = 0; i < indicesToRemove.length; i++) {
-		collidables.splice(indicesToRemove[i],1);
-	}*/
-        
-    if (streak == 10 && !isTurbo) {
-    	console.log('turning on turbo');
-    	if(drunk == 10) {
-    		drunk = 9;
-    	}
-        toasty.toastIt();
-        turboModeOn();
-        //Fire super mode
-    }
-
-    if (drunk == 10 && !isDrunk) {
-        //drunk = 0;
-        setDrunk(drunk);
-        makeDrunkModeGo();
-        //Fire drunk mode
-    }
 }
 
 
@@ -284,4 +290,9 @@ function gameOver() {
     player.stop('BACKGROUND_MUSIC');
     player.stop('TURBO_MUSIC');
     player.play('GAME_OVER');
+    gameOver = 1;
+
+    
+
+    
 }
