7f34b25f54706baf06ed6c181b715e7532d7d4b7
Jackson Willis <jackson@jacksonwillis.com>
2014-02-18 10:17:14 +0000

    adding in nodeAPI dir and files

---
+++ nodeAPI/routes/score.js
@@ -0,0 +1,53 @@
+var mongo = require('mongodb');
+ 
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+ 
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('scoreboarddb', server);
+ 
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'JosHappySuperFunTimeSofaCatchyGame' database");
+
+        //Check for collection and initiate populateDB if non-existant
+        db.collection('scoreboard', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'JosHappySuperFunTimeSofaCatchyGame' collection doesn't exist. Sample data turned OFF...");
+                //populateDB();
+            }
+        });
+    }
+});
+ 
+exports.findAll = function(req, res) {
+    db.collection('scoreboard', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.set({
+                'Access-Control-Allow-Origin': '*'
+            });
+            res.send(items);
+        });
+    });
+};
+ 
+exports.addScore = function(req, res) {
+    var score = req.body;
+
+    db.collection('scoreboard', function(err, collection) {
+        collection.insert(score, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                try {
+                    JSON.stringify(result[0]);
+                    console.log('Success: ' + JSON.stringify(result[0]));
+                    res.send(result[0]);
+                } catch (ex) {
+                    res.send({'error':'NOT JSON'});
+                }
+            }
+        });
+    });
+}
---
+++ nodeAPI/server.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+    score = require('./routes/score');
+ 
+var app = express();
+ 
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
+app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+ 
+app.get('/scores', score.findAll);
+
+app.post('/score', score.addScore);
+ 
+app.listen(3000);
+console.log('JosHappySuperFunTimeSofaCatchyGame nodeJS server started... listening on port 3000');
